c7a813b9869c7cbfa446c9f1f7edf41772148158
musanifali <musanifali300@gmail.com>
2025-09-16 05:27:35 -0700

    Restore full application - fixed white screen issue with proper build config

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import '../src/assets/scss/global.scss';
-import SimpleApp from './SimpleApp';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <SimpleApp />
+    <App />
   </BrowserRouter>
 );
 
